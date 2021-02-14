a40bb4286ebe1a81a57775afe06c9e42efd5e49c
Nobby <nobbyknox@gmail.com>
2021-02-14 10:28:57 +0200

    Use `term_cols` variable in `for` loops
    The two `for` loops that draws the horizontal lines execute the `tput cols` command on each iteration.
    This change uses the `term_cols` variable that was set, but never used.

--- git.sh
+++ git.sh
@@ -3,9 +3,9 @@
 function banner() {
 	term_cols=$(tput cols) 
 	str=":: $1 ::"
-	for ((i=1; i<=`tput cols`; i++)); do echo -n ‾; done
+	for ((i=1; i<=$term_cols; i++)); do echo -n ‾; done
 	tput setaf 10; printf "%*s\n" $(((${#str}+$term_cols)/2)) "$str"; tput sgr0
-	for ((i=1; i<=`tput cols`; i++)); do echo -n _; done
+	for ((i=1; i<=$term_cols; i++)); do echo -n _; done
 	printf "\n"
 }
 
